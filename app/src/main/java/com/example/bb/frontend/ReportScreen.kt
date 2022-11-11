2b24c5975a501e5f4bd894a4399d3ac2afd959f6
Parker Steach <charliebear2821@gmail.com>
2022-11-10 23:35:18 -0600

    Added report functionality

--- app/src/main/java/com/example/bb/frontend/ReportScreen.kt
+++ app/src/main/java/com/example/bb/frontend/ReportScreen.kt
@@ -2,11 +2,11 @@ package com.example.bb.frontend
 
 import android.graphics.Color
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,19 +14,29 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bb.R
 
 @Preview
 @Composable
 fun ReportScreen() {
+    //look into makinga "top of app bar"
+    /*
+    Scaffold(){
+        topBar = { TopAppBar(title = { Text("QQQ | Report Menu",
+            color = androidx.compose.ui.graphics.Color.White) },
+            backgroundColor = androidx.compose.ui.graphics.Color.Green) },
+    }*/
+
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
     ) {
         Text(
-            text = "Reports",
+            text = "Select a Budget to generate a report",
             fontWeight = FontWeight.Bold,
             color = androidx.compose.ui.graphics.Color.White,
             modifier = Modifier.align(Alignment.CenterHorizontally),
@@ -34,24 +44,20 @@ fun ReportScreen() {
             fontSize = 25.sp
         )
 
-        ButtonTest()
+        ButtonTest(        )
     }
 
-   /* Column(
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-
-    )Button(onClick = {
-        //your onclick code here
-    }) {
-        Text(text = "Click ")
-        Text(text = "Here")
-    }
-*/
 }
 
 @Composable
 fun ButtonTest() {
-    Button(onClick = {    }) { Text(text = "Click ") }
+    Button(onClick = {    }) {
+        Text(text = "Generate Report",
+        ) }
+}
+
+@Composable
+fun DropdownMenu(){
+
+
 }
